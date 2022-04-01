44b8806c3a771b341a76ce0ffbedc374d1cb687d
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-01 08:36:55 +0100

    removed unused import

--- lib/screens/widgets/widgets.dart
+++ lib/screens/widgets/widgets.dart
@@ -1,7 +1,5 @@
 import 'package:facebook_clone/config/palette.dart';
-import 'package:facebook_clone/data/data.dart';
 import 'package:facebook_clone/models/models.dart';
-import 'package:flutter/painting.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
