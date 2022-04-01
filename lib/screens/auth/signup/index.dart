aa2abaa0e115a7d076cd381e2e239356da9ef00c
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-01 08:36:21 +0100

    signup index init

---
+++ lib/screens/auth/signup/index.dart
@@ -0,0 +1,35 @@
+import 'package:facebook_clone/screens/auth/index.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignUpIndex extends StatelessWidget {
+  const SignUpIndex({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Create account',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(child: SvgPicture.asset('assets/svg/Illustration.svg'))
+          ],
+        ),
+      ),
+    );
+  }
+}
