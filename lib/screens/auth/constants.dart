6b4fdf47fde5cc1f0d641785aede9ac05df54f01
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-03 15:43:37 +0100

    consts for auth

---
+++ lib/screens/auth/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+TextStyle boldStyle =
+    const TextStyle(fontWeight: FontWeight.bold, fontSize: 18);
+TextStyle lightStyle = const TextStyle(
+    fontWeight: FontWeight.bold, fontSize: 12, color: Color(0XFF999999));
