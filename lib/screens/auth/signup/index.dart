46f5d0457516215a2e64e0ced0e4d017b106584d
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-03 03:01:32 +0100

    hotfix

--- lib/screens/auth/signup/index.dart
+++ lib/screens/auth/signup/index.dart
@@ -1,7 +1,5 @@
-import 'package:facebook_clone/screens/auth/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class SignUpIndex extends StatelessWidget {
   const SignUpIndex({Key? key}) : super(key: key);
@@ -25,10 +23,7 @@ class SignUpIndex extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          children: [
-            SizedBox(
-                height: 100, child: SvgPicture.asset('assets/svg/img_1.svg'))
-          ],
+          children: [SizedBox(child: SvgPicture.asset('assets/svg/img_1.svg'))],
         ),
       ),
     );
