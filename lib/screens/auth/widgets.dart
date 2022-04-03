360cacaeffe5884c251dff4c469ecf9622c75805
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-03 15:44:12 +0100

    button fixes

--- lib/screens/auth/widgets.dart
+++ lib/screens/auth/widgets.dart
@@ -2,10 +2,15 @@ import 'package:facebook_clone/config/palette.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, required this.text, required this.onPressed})
+  const CustomButton(
+      {Key? key,
+      required this.text,
+      required this.onPressed,
+      required this.padding})
       : super(key: key);
   final String text;
   final Function() onPressed;
+  final double padding;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -13,17 +18,37 @@ class CustomButton extends StatelessWidget {
         onTap: onPressed,
         child: Stack(children: [
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+            width: padding,
+            padding: EdgeInsets.symmetric(vertical: 12),
             decoration: BoxDecoration(
                 gradient: Palette.buttonGradient,
                 borderRadius: BorderRadius.circular(24)),
-            child: Text(
-              text,
-              style: const TextStyle(color: Colors.white),
+            child: Center(
+              child: Text(
+                text,
+                style: const TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ]),
       ),
     );
   }
+}
+
+AppBar getAppbar(BuildContext context) {
+  return AppBar(
+    title: const Text(
+      'Create account',
+      style: TextStyle(
+        color: Colors.black,
+      ),
+    ),
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: IconButton(
+      onPressed: () => Navigator.of(context).pop(),
+      icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+    ),
+  );
 }
